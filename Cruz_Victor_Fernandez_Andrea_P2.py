7c91a082e0e74ab850f8b48ec5780a6b4d302b74
Andrea Fernández <andreafernez@estudiantec.cr>
2021-06-06 22:00:34 -0600

    same as the last one
    changes

--- Cruz_Victor_Fernandez_Andrea_P2.py
+++ Cruz_Victor_Fernandez_Andrea_P2.py
@@ -23,6 +23,7 @@ namebox = ""
 #images
 mainscreen = ("./images/background.png")
 
+# global for score 
 score = 0
 
 def menu():
@@ -40,28 +41,34 @@ def menu():
         screen1.blit(background, (0, 0))
 
         # font and fonts´sizes
-        gamefont = pygame.font.Font("./images/mariofont.ttf", 40)
+        gamefont = pygame.font.Font("./images/mariofont.ttf", 50)
         gamefont2 = pygame.font.Font("./images/mariofont.ttf", 20)
+
+        # caption background
+        entryspace = pygame.image.load("./images/entryspace.png").convert_alpha()
+        entryspace = pygame.transform.scale(entryspace, (800, 80))
+        screen1.blit(entryspace, (95, 77))
 
         # displaying the name of the game
         gamename = gamefont.render("M A R I O  I N V A S I O N", 0, (255,255,255))
-        screen1.blit(gamename, (37,95))
+        screen1.blit(gamename, (110, 95))
+
+        # entry space button
+        entryspace = pygame.image.load("./images/entryspace.png").convert_alpha()
+        entryspace = pygame.transform.scale(entryspace, (500, 5))
+        screen1.blit(entryspace, (245, 230))
 
         # name box
         namebox1 = gamefont2.render(namebox, 0, (255, 255, 255))
-        screen1.blit (namebox1, (370, 200))
-
-        # indication to the player to enter their name
-        playername = gamefont2.render("entry your name:", 0, (255,255,255))
-        screen1.blit(playername, (37, 200))
+        screen1.blit (namebox1, (500 - namebox1.get_width()//2, 210))
 
         # levels section
-        levelsection = gamefont2.render("levels", 0, (255,255,255))
-        screen1.blit(levelsection, (250, 350))
+        levelsection = gamefont.render("levels", 0, (222, 89, 24))
+        screen1.blit(levelsection, (160, 350))
 
         # about
-        about = gamefont2.render("about", 0, (255,255,255))
-        screen1.blit(about, (257, 500))
+        about = gamefont.render("about", 0, (222, 89, 24))
+        screen1.blit(about, (640, 350))
 
         # mario image on menu to decorate
         mario = pygame.image.load("./images/right_mario.png").convert_alpha()
@@ -70,16 +77,33 @@ def menu():
         screen1.blit(mario, (190, 480))
 
         # enemies on top of the menu to decorate
-        greenturtle = pygame.image.load("./images/green_turtle.png").convert_alpha()
-        greenturtle = pygame.transform.scale(greenturtle, (50, 50))
-        screen1.blit(greenturtle, (510, 10))
-        grayturtle = pygame.image.load("./images/gray_turtle.png").convert_alpha()
-        grayturtle = pygame.transform.scale(grayturtle, (50, 50))
-        screen1.blit(grayturtle, (450, 10))
+        greenturtle1 = pygame.image.load("./images/green_turtle1.png").convert_alpha()
+        greenturtle1 = pygame.transform.scale(greenturtle1, (50, 50))
+        screen1.blit(greenturtle1, (538, 10))
+        grayturtle1 = pygame.image.load("./images/gray_turtle1.png").convert_alpha()
+        grayturtle1 = pygame.transform.scale(grayturtle1, (50, 50))
+        screen1.blit(grayturtle1, (418, 10))
         greenwizard = pygame.image.load("./images/green_cloud.png").convert_alpha()
         greenwizard = pygame.transform.scale(greenwizard, (50, 50))
-        screen1.blit(greenwizard, (390, 10))
-        
+        screen1.blit(greenwizard, (358, 10))
+        brownturtle1 = pygame.image.load("./images/brown_turtle1.png").convert_alpha()
+        brownturtle1 = pygame.transform.scale(brownturtle1, (50, 50))
+        screen1.blit(brownturtle1, (298, 10))
+        browncloud = pygame.image.load("./images/brown_cloud.png").convert_alpha()
+        browncloud = pygame.transform.scale(browncloud, (50, 50))
+        screen1.blit(browncloud, (478, 10))
+        graycloud = pygame.image.load("./images/gray_cloud.png").convert_alpha()
+        graycloud = pygame.transform.scale(graycloud, (50, 50))
+        screen1.blit(graycloud, (598, 10))
+        redturtle1 = pygame.image.load("./images/red_turtle1.png").convert_alpha()
+        redturtle1 = pygame.transform.scale(redturtle1, (50, 50))
+        screen1.blit(redturtle1, (658, 10))
+        whitebullet = pygame.image.load("./images/white_bullet.png").convert_alpha()
+        whitebullet = pygame.transform.scale(whitebullet, (50, 50))
+        screen1.blit(whitebullet, (238, 10))
+        cyanbullet = pygame.image.load("./images/cyan_bullet.png").convert_alpha()
+        cyanbullet = pygame.transform.scale(cyanbullet, (50, 50))
+        screen1.blit(cyanbullet, (718, 10))
 
         # images of the level buttons
         buttonlevel1 = gamefont2.render("level 1", 0, (255,255,255))
@@ -147,12 +171,7 @@ def menu():
 
         # entry name button
         buttonentry = gamefont.render("entry name", 0, (255,255,255))
-        screen1.blit(buttonentry, (330, 240))
-
-        # entry space button
-        entryspace = pygame.image.load("./images/entryspace.png").convert_alpha()
-        entryspace = pygame.transform.scale(entryspace, (400, 5))
-        screen1.blit(entryspace, (275, 230))
+        screen1.blit(buttonentry, (320, 240))
 
         # info button
         infobutton = gamefont2.render("info", 0, (255,255,255))
